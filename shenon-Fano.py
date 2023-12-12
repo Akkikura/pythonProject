5259914d578f34a333bb535eed1a13bfdceb6de5
Akkikura <sergon.d.ru@gmail.com>
2023-12-12 18:44:07 +0300

    final for fano

--- shenon-Fano.py
+++ shenon-Fano.py
@@ -1,11 +1,11 @@
 from Main import text
 from pprint import pprint
-
+import math
 
 class Node:
-    def __init__(self, symbol=None, probability=None):
+    def __init__(self, symbol=None, frequency=None):
         self.symbol = symbol
-        self.probability = probability
+        self.frequency = frequency
         self.left = None
         self.right = None
 
@@ -13,31 +13,38 @@ class Node:
 def calculate_probabilities(text, symbols_to_encode):
     filtered_text = ''.join(char for char in text if char in symbols_to_encode)
 
-    symbol_frequencies = [(symbol, filtered_text.count(symbol)) for symbol in set(filtered_text)]
-    symbol_frequencies = sorted(symbol_frequencies, key=lambda x: x[1], reverse=True)
-
     total_characters = len(filtered_text)
-    probabilities = [(symbol, frequency / total_characters) for symbol, frequency in symbol_frequencies]
+    symbol_frequencies = {char: filtered_text.count(char) / total_characters for char in set(filtered_text)}
 
-    return probabilities
+    return symbol_frequencies
 
 
-def build_shannon_fano_tree(probabilities):
-    if len(probabilities) == 1:
-        return Node(symbol=probabilities[0][0], probability=probabilities[0][1])
+def calculate_entropy(probabilities):
+    entropy = -sum(prob * math.log2(prob) for prob in probabilities.values())
+    return entropy
 
-    total_probability = sum(symbol[1] for symbol in probabilities)
-    cumulative_probability = 0
+
+def shannon_fano_average_length(codes, probabilities):
+    avg_length = sum(len(codes[char]) * probabilities[char] for char in codes)
+    return avg_length
+
+
+def build_shannon_fano_tree(symbols):
+    if len(symbols) == 1:
+        return Node(symbol=symbols[0][0], frequency=symbols[0][1])
+
+    total_frequency = sum(symbol[1] for symbol in symbols)
+    cumulative_frequency = 0
     split_index = 0
 
-    for i, symbol in enumerate(probabilities):
-        cumulative_probability += symbol[1]
-        if cumulative_probability * 2 >= total_probability:
+    for i, symbol in enumerate(symbols):
+        cumulative_frequency += symbol[1]
+        if cumulative_frequency * 2 >= total_frequency:
             split_index = i
             break
 
-    left_branch = build_shannon_fano_tree(probabilities[:split_index + 1])
-    right_branch = build_shannon_fano_tree(probabilities[split_index + 1:])
+    left_branch = build_shannon_fano_tree(symbols[:split_index + 1])
+    right_branch = build_shannon_fano_tree(symbols[split_index + 1:])
 
     node = Node()
     node.left = left_branch
@@ -61,12 +68,14 @@ def generate_shannon_fano_codes(node, code="", code_dict=None):
 
 def shannon_fano_encode(text, symbols_to_encode):
     probabilities = calculate_probabilities(text, symbols_to_encode)
-    root = build_shannon_fano_tree(probabilities)
+    root = build_shannon_fano_tree(sorted(probabilities.items(), key=lambda x: x[1], reverse=True))
     codes = generate_shannon_fano_codes(root)
 
     encoded_text = ''.join(codes[symbol] for symbol in text if symbol in symbols_to_encode)
 
-    return encoded_text, codes
+    return encoded_text, codes, probabilities
+
+
 def shannon_fano_decode(encoded_text, codes):
     reverse_codes = {code: symbol for symbol, code in codes.items()}
 
@@ -81,11 +90,23 @@ def shannon_fano_decode(encoded_text, codes):
 
     return decoded_text
 
+
+# Пример использования
+huge_text = "..."  # Ваш огромный текст здесь
+symbols_to_encode = "0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюя.,:;-"
+
+encoded_result, codes, probabilities = shannon_fano_encode(huge_text, symbols_to_encode)
+
+entropy = calculate_entropy(probabilities)
+avg_length = shannon_fano_average_length(codes, probabilities)
+
 huge_text = text
 symbols_to_encode = '0123456789абвгдеёжзийклмнопрстуфхцчшщъыьэюя.,:;- ('
 
-encoded_result, codes = shannon_fano_encode(huge_text, symbols_to_encode)
-probabilities = calculate_probabilities(text, symbols_to_encode)
+encoded_result, codes, probabilities = shannon_fano_encode(huge_text, symbols_to_encode)
+
+entropy = calculate_entropy(probabilities)
+avg_length = shannon_fano_average_length(codes, probabilities)
 
 # pprint(f'Исходный текст: {huge_text}')
 # pprint(f'Закодированный текст: {encoded_result}')
@@ -94,4 +115,6 @@ probabilities = calculate_probabilities(text, symbols_to_encode)
 # decoded_result = shannon_fano_decode(encoded_result, codes)
 # pprint(f'Декодированный текст: {decoded_result}')
 
-pprint(probabilities)
+print("Коды символов Шеннона-Фано:", codes)
+print("Энтропия текста:", entropy)
+print("Среднее количество двоичных разрядов:", avg_length)
